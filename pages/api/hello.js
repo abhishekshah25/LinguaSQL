3b14a6e6c6ea1c1a4c4dbf2f53f6d680350cd75f
abhishekshah25 <147745895+abhishekshah25@users.noreply.github.com>
2023-10-19 10:54:00 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction 
+
+// Sample Dummy Code
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
